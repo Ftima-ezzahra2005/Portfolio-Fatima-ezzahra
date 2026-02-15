8f16eeab94162b60c8f38ae7621b4cdbb1b928db
Ftima-ezzahra2005 <zaghloulfatimaezzahra712@gmail.com>
2026-02-15 21:44:06 +0000

    Add files via upload

---
+++ portfolio1.js
@@ -0,0 +1,15 @@
+/* Scroll Animation */
+
+const cards = document.querySelectorAll(".card");
+const skills = document.querySelectorAll(".skill-box");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+  });
+},{threshold:0.2});
+
+cards.forEach(card => observer.observe(card));
+skills.forEach(skill => observer.observe(skill));
